5b1f97ad11a695f83f31f9ff59149736ef357cd8
Thomas Swope <35576679+tjswope@users.noreply.github.com>
2025-02-05 12:00:56 -0500

    removed bombs

--- MineSweeper/src/GraphicsPanel.java
+++ MineSweeper/src/GraphicsPanel.java
@@ -65,18 +65,7 @@ public class GraphicsPanel extends JPanel implements MouseListener{
     }
 
     private void placeBombs(int bombCount) {
-        Random rand = new Random();
-        int placedBombs = 0;
-
-        while (placedBombs < bombCount) {
-            int row = rand.nextInt(ROWS);
-            int col = rand.nextInt(COLS);
-
-            if (!cells[row][col].hasBomb()) {
-                cells[row][col].setBomb();
-                placedBombs++;
-            }
-        }
+        cells[0][0].setBomb();
     }
 
     private void calculateNeighborBombCounts() {
