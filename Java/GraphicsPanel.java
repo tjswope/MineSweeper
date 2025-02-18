a4fd172e2e4f9a81eb44feaca0bf16c21c4d53e8
Thomas Swope <35576679+tjswope@users.noreply.github.com>
2025-02-18 10:37:59 -0500

    Update GraphicsPanel.java

--- Java/GraphicsPanel.java
+++ Java/GraphicsPanel.java
@@ -19,12 +19,12 @@ public class GraphicsPanel extends JPanel implements MouseListener{
         // Initialize the Cell objects
         for (int row = 0; row < ROWS; row++) {
             for (int col = 0; col < COLS; col++) {
-                cells[row][col] = new Cell(col * 30, row * 30); // Assuming 32x32 cell size
+                cells[row][col] = new Cell(col * 30, row * 30); // Assuming 30x30 cell size
             }
         }
 
         // Randomly place bombs and calculate neighbor bomb counts
-        placeBombs(10); // Place 10 bombs
+        placeBombs(); 
         calculateNeighborBombCounts();
     }
 
@@ -64,7 +64,9 @@ public class GraphicsPanel extends JPanel implements MouseListener{
         }
     }
 
-    private void placeBombs(int bombCount) {
+    // this function should randomly place NUM_BOMBS on the board.
+    // right now it only places one bomb in the top left corner.
+    private void placeBombs() {
 
     }
 
@@ -77,57 +79,32 @@ public class GraphicsPanel extends JPanel implements MouseListener{
         }
     }
 
+    // this function should count how many bombs sorround the cell located at [row][col]
     private int countNeighborBombs(int row, int col) {
         int count = 0;
-
-        for (int i = -1; i <= 1; i++) {
-            for (int j = -1; j <= 1; j++) {
-                int neighborRow = row + i;
-                int neighborCol = col + j;
-
-                if (neighborRow >= 0 && neighborRow < ROWS && neighborCol >= 0 && neighborCol < COLS) {
-                    if (cells[neighborRow][neighborCol].hasBomb()) {
-                        count++;
-                    }
-                }
-            }
-        }
 
         return count;
     }
 
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void mouseClicked(MouseEvent e) {}
 
-	@Override
-	public void mousePressed(MouseEvent e) {
-		if (e.getButton() == MouseEvent.BUTTON1) {
+    @Override
+    public void mousePressed(MouseEvent e) {
+	if (e.getButton() == MouseEvent.BUTTON1) {
             System.out.println("Left button clicked");
             revealCell(e.getY()/30, e.getX()/30);
         } else if (e.getButton() == MouseEvent.BUTTON3) {
             System.out.println("Right button clicked");
-        }
-		
-	}
+        }		
+    }
 
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void mouseReleased(MouseEvent e) {}
 
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void mouseEntered(MouseEvent e) {}
 
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void mouseExited(MouseEvent e) {}
 }
